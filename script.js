b11a92c95ffd7dd8189284a827ef3fddbd26a0d4
Kaviya182005 <161860620+Kaviya182005@users.noreply.github.com>
2024-03-25 18:25:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// Smooth Scrolling
+$(document).ready(function() {
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+
+// Form Validation (You can add more validation as needed)
+function validateForm() {
+    var name = document.forms["contactForm"]["name"].value;
+    var email = document.forms["contactForm"]["email"].value;
+    var message = document.forms["contactForm"]["message"].value;
+
+    if (name == "" || email == "" || message == "") {
+        alert("Please fill in all fields.");
+        return false;
+    }
+}
